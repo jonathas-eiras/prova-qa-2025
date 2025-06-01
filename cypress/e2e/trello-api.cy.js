24de44a56acd31f9145150c9b8afc04053e1815f
jhoneiras <jhon4eiras@gmail.com>
2025-06-01 18:12:43 -0300

    Melhoras nos asserts

--- cypress/e2e/trello-api.cy.js
+++ cypress/e2e/trello-api.cy.js
@@ -4,43 +4,51 @@ describe('Testes com a API do Trello via Cypress Commands', () => {
   let listId = '';
 
   it('Criar um board', () => {
-    cy.createBoard('Board de Teste').then((id) => {
-      boardId = id;
+    cy.createBoard('Board de Teste').then((response) => {
+      boardId = response.body.id;
+      expect(response.status).to.equal(200);
+      cy.deleteBoard(boardId);
     });
+
   });
 
   it('Excluir um board', () => {
-    cy.createBoard('Board de Teste').then((id) => {
-      boardId = id;
+    cy.createBoard('Board de Teste').then((response) => {
+      boardId = response.body.id;
+      cy.deleteBoard(boardId).then((response) => {
+        expect(response.status).to.equal(200)
+      });
     });
-    cy.deleteBoard(boardId);
   });
 
 
   it('Criar um card', () => {
-    cy.createBoard('Board para Card').then((id) => {
-      boardId = id;
+    cy.createBoard('Board para Card').then((response) => {
+      boardId = response.body.id;
       cy.getListsFromBoard(boardId).then((lists) => {
         listId = lists[0].id;
-        cy.createCard(listId, 'Meu Card').then((card) => {
-          cardId = card.id;
+        cy.createCard(listId, 'Meu Card').then((response) => {
+          cardId = response.body.id;
+          expect(response.status).to.equal(200)
         });
       });
+      cy.deleteBoard(boardId);
     });
-    cy.deleteBoard(boardId);
   });
 
   it('Excluir um card', () => {
-    cy.createBoard('Board para Card').then((id) => {
-      boardId = id;
+    cy.createBoard('Board para Card').then((response) => {
+      boardId = response.body.id;
       cy.getListsFromBoard(boardId).then((lists) => {
         listId = lists[0].id;
-        cy.createCard(listId, 'Meu Card').then((card) => {
-          cardId = card.id;
-          cy.deleteCard(cardId);
-          cy.deleteBoard(boardId);
+        cy.createCard(listId, 'Meu Card').then((response) => {
+          cardId = response.body.id;
+          cy.deleteCard(cardId).then((response) => {
+            expect(response.status).to.equal(200);
+          });
         });
       });
+      cy.deleteBoard(boardId);
     });
   });
 });
